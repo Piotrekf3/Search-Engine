f85721321ad6ac52bf0a9e25e75abe24cbb78033
Piotrekf3 <piotrek.fur@wp.pl>
2018-06-03 15:32:05 +0200

    indexer

---
+++ src/main/java/Indexer.java
@@ -0,0 +1,20 @@
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Indexer {
+    private IndexReader indexReader;
+
+    public Indexer() {
+        try {
+            indexReader = DirectoryReader.open(FSDirectory.open(Paths.get("data/index")));
+        }
+        catch(IOException e) {
+            System.err.println("Error opening index" + e);
+            e.printStackTrace();
+        }
+    }
+}
